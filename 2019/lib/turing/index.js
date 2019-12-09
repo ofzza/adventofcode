c0fd2d8bec26ee8530b44abe37829456faf1b397
ofzza <ofzza@ofzza.com>
2019-12-09 09:57:28 +0100

    2019/09

--- 2019/lib/turing/index.js
+++ 2019/lib/turing/index.js
@@ -10,28 +10,37 @@ const instructions = [
   { opcode: 5, params: 2, func: 'jump-true' },
   { opcode: 6, params: 2, func: 'jump-false' },
   { opcode: 7, params: 3, func: 'less-than' },
-  { opcode: 8, params: 3, func: 'equals' }
+  { opcode: 8, params: 3, func: 'equals' },
+  { opcode: 9, params: 1, func: 'set-rel-base' }
 ];
 
 // Computer as output generator implementation
 module.exports.run = function * run (prog, inputs = [], actions = []) {
-  const output = []
-  let inputCounter = 0;
+  // Set relative-base pointer value
+  let relbase = 0;
+  // Run program
   loopProg: for (let i=0; prog[i] !== 99 && prog[i] !== undefined;) {
     // Parse opcode
     const instruction = instructions[prog[i] % 100],
           modes = Math.round(prog[i] / 100).toString().padStart(instruction.params, '0').split('').reverse().map((n) => { return parseInt(n); });
     // Run extensions
     for (let action of actions) {
-      let output = action(prog, i, instruction, modes, inputs);
-      // Check for output value
-      if (output !== undefined && output.output !== undefined) {
-        yield output.output;
-      }
-      // Check for jump
-      if (output !== undefined && output.jump) {
-        i = output.jump;
-        continue loopProg;
+      const args = getArguments({ prog, i, instruction, modes, relbase }),
+            output = action({ prog, instruction, args, inputs });
+      if (output !== undefined) {
+        // Check for output value
+        if (output.output !== undefined) {
+          yield output.output;
+        }
+        // Check for jump
+        if (output.jump !== undefined) {
+          i = output.jump;
+          continue loopProg;
+        }
+        // Check for relative base update
+        if (output.relbase !== undefined) {
+          relbase += output.relbase;
+        }
       }
     }
     // Move to next opcode
@@ -41,48 +50,78 @@ module.exports.run = function * run (prog, inputs = [], actions = []) {
 
 // Turing actions
 module.exports.actions = {
-  turingAdd: function turingAdd (prog, i, instruction, modes, inputs) {
+  // Add
+  turingAdd: function turingAdd ({ prog, instruction, args } = {}) {
     if (instruction.func === 'add') {
-      prog[!modes[2] ? prog[i+3] : (i+3)] = (!modes[0] ? prog[prog[i+1]] : prog[i+1]) + (!modes[1] ? prog[prog[i+2]] : prog[i+2]);
+      prog[args[2].index] = args[0].value + args[1].value;
     }
   },
-  turingMultiply: function turingMultiply (prog, i, instruction, modes, inputs) {
+  // Multiply
+  turingMultiply: function turingMultiply ({ prog, instruction, args } = {}) {
     if (instruction.func === 'multiply') {
-      prog[!modes[2] ? prog[i+3] : (i+3)] = (!modes[0] ? prog[prog[i+1]] : prog[i+1]) * (!modes[1] ? prog[prog[i+2]] : prog[i+2]);
+      prog[args[2].index] = args[0].value * args[1].value;
     }
   },
-  turingInput: function turingInput (prog, i, instruction, modes, inputs) {
+  // Read from input
+  turingInput: function turingInput ({ prog, instruction, args, inputs } = {}) {
     if (instruction.func === 'input') {
-      prog[!modes[0] ? prog[i+1] : (i+1)] = inputs.splice(0, 1)[0];
+      prog[args[0].index] = inputs.splice(0, 1)[0];
     }
   },
-  turingOutput: function turingOutput (prog, i, instruction, modes, inputs) {
+  // Write to output
+  turingOutput: function turingOutput ({ instruction, args } = {}) {
     if (instruction.func === 'output') {
-      return { output: prog[!modes[0] ? prog[i+1] : (i+1)] };
+      return { output: args[0].value };
     }
   },
-  turingJumpTrue: function turingJumpTrue (prog, i, instruction, modes, inputs) {
+  // Jump if true
+  turingJumpTrue: function turingJumpTrue ({ instruction, args } = {}) {
     if (instruction.func === 'jump-true') {
-      if (!modes[0] ? prog[prog[i+1]] : prog[i+1]) {
-        return { jump: !modes[1] ? prog[prog[i+2]] : prog[i+2] };
+      if (args[0].value) {
+        return { jump: args[1].value };
       }
     }
   },
-  turingJumpFalse: function turingJumpFalse (prog, i, instruction, modes, inputs) {
+  // Jump if false
+  turingJumpFalse: function turingJumpFalse ({ instruction, args } = {}) {
     if (instruction.func === 'jump-false') {
-      if (!(!modes[0] ? prog[prog[i+1]] : prog[i+1])) {
-        return { jump: !modes[1] ? prog[prog[i+2]] : prog[i+2] };
+      if (!args[0].value) {
+        return { jump: args[1].value };
       }
     }
   },
-  turingLessThan: function turingLessThan (prog, i, instruction, modes, inputs) {
+  // Check if less-than
+  turingLessThan: function turingLessThan ({ prog, instruction, args } = {}) {
     if (instruction.func === 'less-than') {
-      prog[!modes[2] ? prog[i+3] : (i+3)] = ((!modes[0] ? prog[prog[i+1]] : prog[i+1]) < (!modes[1] ? prog[prog[i+2]] : prog[i+2]) ? 1 : 0);
+      prog[args[2].index] = (args[0].value < args[1].value ? 1 : 0);
     }
   },
-  turingEquals: function turingEquals (prog, i, instruction, modes, inputs) {
+  // Check if equals
+  turingEquals: function turingEquals ({ prog, instruction, args } = {}) {
     if (instruction.func === 'equals') {
-      prog[!modes[2] ? prog[i+3] : (i+3)] = ((!modes[0] ? prog[prog[i+1]] : prog[i+1]) === (!modes[1] ? prog[prog[i+2]] : prog[i+2]) ? 1 : 0);
+      prog[args[2].index] = (args[0].value === args[1].value ? 1 : 0);
+    }
+  },
+  // Update relative base pointer
+  turingSetRelativeBase: function turingEquals ({ instruction, args } = {}) {
+    if (instruction.func === 'set-rel-base') {
+      return { relbase: args[0].value };
     }
   }
 };
+
+// Get argument values
+function getArguments ({ prog, i, instruction, modes, relbase } = {}) {
+  return [...Array(instruction.params)].map((u, j) => {
+    if (modes[j] === 0) {
+      // Read in absolute mode
+      return { index: (prog[i + j + 1] || 0), value: (prog[prog[i + j + 1] || 0] || 0) };
+    } else if (modes[j] === 1) {
+      // Read in parameter mode
+      return { index: (i + j + 1), value: (prog[i + j + 1] || 0) };
+    } else if (modes[j] === 2) {
+      // Read in relative mode
+      return { index: (relbase + (prog[i + j + 1] || 0)), value: (prog[relbase + (prog[i + j + 1] || 0)] || 0) };
+    }
+  });
+}
---
+++ 2019/puzzles/day09/index.js
@@ -0,0 +1,46 @@
+// DAY 09
+// https://adventofcode.com/2019/day/9
+
+// Import dependencies
+const puzzle = require('../../../lib').puzzle,
+      turing  = require('../../lib/turing');
+
+// Set global inputs
+const prog = require('fs').readFileSync(require('path').join(__dirname, './input.txt')).toString().trim().split(',').map(a => parseInt(a));;
+
+// 1st puzzle of the day
+function puzzle01 (...args) {
+  const prog = args[0],
+        inputs = args[1];
+  return [
+    ...turing.run(prog, inputs, [
+      turing.actions.turingAdd,
+      turing.actions.turingMultiply,
+      turing.actions.turingInput,
+      turing.actions.turingOutput,
+      turing.actions.turingJumpTrue,
+      turing.actions.turingJumpFalse,
+      turing.actions.turingLessThan,
+      turing.actions.turingEquals,
+      turing.actions.turingSetRelativeBase
+    ])
+  ];
+}
+module.exports.puzzle01 = () => {
+  puzzle('2019', '09', '01', puzzle01, [
+    [[109,1,204,-1,1001,100,1,100,1008,100,16,101,1006,101,0,99], []],  { expected: [109,1,204,-1,1001,100,1,100,1008,100,16,101,1006,101,0,99] },
+    [[1102,34915192,34915192,7,4,7,99,0], []],                          { expected: [1219070632396864] },
+    [[104,1125899906842624,99], []],                                    { expected: [1125899906842624] },
+    [[...prog], [1]],                                                   { expected: [3601950151], test: false },
+  ]);
+};
+
+// 2nd puzzle of the day
+function puzzle02 (...args) {
+  return puzzle01(...args);
+}
+module.exports.puzzle02 = () => {
+  puzzle('2019', '09', '02', puzzle02, [
+    [[...prog], [2]], { expected: undefined, test: false },
+  ]);
+};
